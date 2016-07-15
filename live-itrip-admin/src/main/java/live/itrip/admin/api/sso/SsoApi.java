5976276c40086e492ce26d4044cb287d578681b0
fengjianfeng <fengjianfeng@testin.cn>
2016-07-15 17:08:47 +0800

    1. common 添加类库

--- live-itrip-admin/src/main/java/live/itrip/admin/api/sso/SsoApi.java
+++ live-itrip-admin/src/main/java/live/itrip/admin/api/sso/SsoApi.java
@@ -1,6 +1,9 @@
 package live.itrip.admin.api.sso;
 
+import com.alibaba.fastjson.JSON;
 import live.itrip.admin.api.AbstractApi;
+import live.itrip.admin.api.sso.bean.LoginRequest;
+import live.itrip.admin.api.sso.bean.LoginResponse;
 
 /**
  * Created by Feng on 2016/7/14.
@@ -8,8 +11,23 @@ import live.itrip.admin.api.AbstractApi;
 public class SsoApi extends AbstractApi {
     private static final String USER_ACTION = "user.action";
 
-    public String userLogin() {
+    /**
+     * 用户登录
+     *
+     * @param email
+     * @param password
+     * @return
+     */
+    public LoginResponse userLogin(String email, String password) {
 
-        return postJsonString(new Object(), USER_ACTION);
+        LoginRequest loginRequest = new LoginRequest();
+        LoginRequest.LoginData data = new LoginRequest.LoginData();
+        data.setEmail(email);
+        data.setPassword(password);
+
+        String res = postJsonString(loginRequest, USER_ACTION);
+        LoginResponse loginResponse = JSON.parseObject(res, LoginResponse.class);
+
+        return loginResponse;
     }
 }
