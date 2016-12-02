f311ffb70a6d7e22009b91cd41464b8092f88797
NameJianF <fjf789@126.com>
2016-12-02 19:09:46 +0800

    1. -------------- 完善页面 ... --------------

--- live-itrip-admin/src/main/webapp/javascript/viewcommon.js
+++ live-itrip-admin/src/main/webapp/javascript/viewcommon.js
@@ -34,7 +34,6 @@ function execAjaxDataForView(url, jsondata, async, error, success, complete) {
     });
 }
 
-
 function getProductsHtml(jsonarray) {
     var content = "";
     for (var i = 0; i < jsonarray.length; i++) {
@@ -70,3 +69,61 @@ function getProductsHtml(jsonarray) {
 
     return content;
 }
+
+function initNavbar() {
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 80
+    });
+
+    // Page scrolling feature
+    $('a.page-scroll').bind('click', function (event) {
+        var link = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(link.attr('href')).offset().top - 50
+        }, 500);
+        event.preventDefault();
+        $("#navbar").collapse('hide');
+    });
+
+    var cbpAnimatedHeader = (function () {
+        var docElem = document.documentElement,
+            header = document.querySelector('.navbar-default'),
+            didScroll = false,
+            changeHeaderOn = 200;
+
+        function init() {
+            window.addEventListener('scroll', function (event) {
+                if (!didScroll) {
+                    didScroll = true;
+                    setTimeout(scrollPage, 250);
+                }
+            }, false);
+        }
+
+        function scrollPage() {
+            var sy = scrollY();
+            if (sy >= changeHeaderOn) {
+                $(header).addClass('navbar-scroll')
+            }
+            else {
+                $(header).removeClass('navbar-scroll')
+            }
+            didScroll = false;
+        }
+
+        function scrollY() {
+            return window.pageYOffset || docElem.scrollTop;
+        }
+
+        init();
+
+    })();
+
+    // Activate WOW.js plugin for animation on scrol
+    new WOW().init();
+
+}
+
+
