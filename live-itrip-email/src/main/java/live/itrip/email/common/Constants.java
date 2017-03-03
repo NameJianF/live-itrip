9de5e2f3ab4a2c183841dd4f14927fd96eb6bb51
NameJianF <fjf789@126.com>
2017-03-03 18:42:19 +0800

    1. 实现邮件服务功能

---
+++ live-itrip-email/src/main/java/live/itrip/email/common/Constants.java
@@ -0,0 +1,16 @@
+package live.itrip.email.common;
+
+
+/**
+ * Created by Feng on 2017/2/27.
+ */
+public class Constants {
+
+    public class Email_Status {
+        //        状态：0-创建；1-发送中； 2-发送完成；3-发送失败
+        public static final String INIT = "0";
+        public static final String SENDING = "1";
+        public static final String SUCCESS = "2";
+        public static final String FAILD = "3";
+    }
+}
--- live-itrip-email/src/main/java/live/itrip/email/service/impls/SendEmailService.java
+++ live-itrip-email/src/main/java/live/itrip/email/service/impls/SendEmailService.java
@@ -1,11 +1,17 @@
 package live.itrip.email.service.impls;
 
+import com.alibaba.fastjson.JSON;
+import live.itrip.common.ErrorCode;
+import live.itrip.common.Logger;
 import live.itrip.common.response.BaseResult;
+import live.itrip.email.common.Config;
+import live.itrip.email.common.Constants;
 import live.itrip.email.dao.SendEmailMapper;
 import live.itrip.email.model.MailSenderInfo;
 import live.itrip.email.service.BaseService;
 import live.itrip.email.service.interfaces.ISendEmailService;
 import live.itrip.email.utils.SimpleMailSender;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.stereotype.Service;
@@ -17,6 +23,8 @@ import java.util.concurrent.BlockingQueue;
 
 /**
  * Created by Feng on 2017/2/24.
+ * <p/>
+ * 1. email 发送服务
  */
 @Service
 public class SendEmailService extends BaseService implements ISendEmailService {
@@ -42,9 +50,23 @@ public class SendEmailService extends BaseService implements ISendEmailService {
     private void send() {
         while (!stop) {
             try {
+
                 MailSenderInfo entity = this.blockingQueue.take();
                 boolean result = SimpleMailSender.send(entity);
-                Thread.sleep(10);
+                if (result) {
+                    // update db
+                    entity.setStatus(Constants.Email_Status.SUCCESS);
+                } else {
+                    // TODO 添加发送失败策略
+
+                    // update db
+                    entity.setStatus(Constants.Email_Status.FAILD);
+                }
+                entity.setSendTimes(entity.getSendTimes() + 1);
+                entity.setUpdateTime(System.currentTimeMillis());
+                this.sendEmailMapper.updateByPrimaryKey(entity);
+
+                Thread.sleep(5);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
@@ -55,7 +77,7 @@ public class SendEmailService extends BaseService implements ISendEmailService {
         try {
             blockingQueue.put(entity);
         } catch (Exception ex) {
-            ex.printStackTrace();
+            Logger.error(ex.getMessage(), ex);
         }
     }
 
@@ -64,8 +86,36 @@ public class SendEmailService extends BaseService implements ISendEmailService {
     public void sendEmail(String decodeJson, HttpServletResponse response, HttpServletRequest request) {
         BaseResult result = new BaseResult();
 
+        if (StringUtils.isEmpty(Config.SERVER_HOST)
+                || StringUtils.isEmpty(Config.SERVER_PORT)
+                || StringUtils.isEmpty(Config.SERVER_USER_NAME)
+                || StringUtils.isEmpty(Config.SERVER_USER_PWD)) {
+            result.setError(ErrorCode.SERVICE_INITING);
+            this.writeResponse(response, result);
+            return;
+        }
 
 
+        MailSenderInfo senderInfo = JSON.parseObject(decodeJson, MailSenderInfo.class);
+
+        if (senderInfo != null) {
+            senderInfo.setStatus(Constants.Email_Status.INIT);
+            senderInfo.setServerHost(Config.SERVER_HOST);
+            senderInfo.setServerPort(Config.SERVER_PORT);
+            senderInfo.setUserName(Config.SERVER_USER_NAME);
+            senderInfo.setSendTimes(0);
+            senderInfo.setCreateTime(System.currentTimeMillis());
+
+            // save to db
+            int ret = this.sendEmailMapper.insertSelective(senderInfo);
+
+            if (ret > 0) {
+                // add to blockingQueue
+                this.put(senderInfo);
+            }
+        }
+
+        result.setError(ErrorCode.SUCCESS);
         this.writeResponse(response, result);
     }
 }
